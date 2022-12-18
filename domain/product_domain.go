f6e17c6f89733da3d9c14b1837f76cd9bac740b0
umar <umar.dev500@gmail.com>
2022-12-18 14:14:38 +0700

    change method sequence

--- domain/product_domain.go
+++ domain/product_domain.go
@@ -6,14 +6,14 @@ import "membership/pb"
 type ProductUsecase interface {
 	// Post creates a new product.
 	Post(req *pb.ProductCreateRequest) error
-	// Delete removes a product from the repository.
-	Delete(req *pb.ProductDeleteRequest) (res *pb.OperationResponse, err error)
 	// FindOne retrieves a single product from the repository.
 	FindOne(req *pb.ProductFindOneRequest) (res *pb.Product, err error)
 	// FindAll retrieves all products from the repository.
 	FindAll(req *pb.ProductFindAllRequest) (res *pb.ProductFindAllResponse, err error)
 	// Update updates a product in the repository.
 	Update(req *pb.ProductUpdateRequest) (res *pb.OperationResponse, err error)
+	// Delete removes a product from the repository.
+	Delete(req *pb.ProductDeleteRequest) (res *pb.OperationResponse, err error)
 }
 
 // ProductRepository is an interface that defines the methods for interacting with a repository of products.
