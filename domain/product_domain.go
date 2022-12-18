160aec82d32b26b2192256049d9737d19a92b999
umar <umar.dev500@gmail.com>
2022-12-18 13:42:32 +0700

    add comments

--- domain/product_domain.go
+++ domain/product_domain.go
@@ -2,17 +2,28 @@ package domain
 
 import "membership/pb"
 
+// ProductUsecase is an interface that defines the methods for interacting with products.
 type ProductUsecase interface {
+	// Post creates a new product.
 	Post(req *pb.ProductCreateRequest) error
+	// Delete removes a product from the repository.
 	Delete(req *pb.ProductDeleteRequest) (res *pb.OperationResponse, err error)
+	// FindOne retrieves a single product from the repository.
 	FindOne(req *pb.ProductFindOneRequest) (res *pb.Product, err error)
+	// FindAll retrieves all products from the repository.
 	FindAll(req *pb.ProductFindAllRequest) (res *pb.ProductFindAllResponse, err error)
 }
 
+// ProductRepository is an interface that defines the methods for interacting with a repository of products.
 type ProductRepository interface {
+	// Post creates a new product in the repository.
 	Post(req *pb.ProductCreateRequest) error
+	// Delete removes a product from the repository.
 	Delete(req *pb.ProductDeleteRequest) (res *pb.OperationResponse, err error)
+	// FindOne retrieves a single product from the repository.
 	FindOne(req *pb.ProductFindOneRequest) (res *pb.Product, err error)
+	// FindAll retrieves all products from the repository.
 	FindAll(req *pb.ProductFindAllRequest) (res *pb.ProductFindAllResponse, err error)
+	// Update updates a product in the repository.
 	Update(req *pb.ProductUpdateRequest) (res *pb.OperationResponse, err error)
 }
