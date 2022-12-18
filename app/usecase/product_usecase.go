d33e5cd9bc26cfa61d18d9046daba8e2b6593447
umar <umar.dev500@gmail.com>
2022-12-18 14:13:51 +0700

    change method sequence

--- app/usecase/product_usecase.go
+++ app/usecase/product_usecase.go
@@ -16,23 +16,22 @@ func NewProductUsecase(repository domain.ProductRepository) domain.ProductUsecas
 }
 
 // func (p *ProductUsecase) {}
-
-func (p *ProductUsecase) Update(req *pb.ProductUpdateRequest) (res *pb.OperationResponse, err error) {
-	return p.repository.Update(req)
-}
-
-func (p *ProductUsecase) FindAll(req *pb.ProductFindAllRequest) (*pb.ProductFindAllResponse, error) {
-	return p.repository.FindAll(req)
+func (p *ProductUsecase) Post(req *pb.ProductCreateRequest) error {
+	return p.repository.Post(req)
 }
 
 func (p *ProductUsecase) FindOne(req *pb.ProductFindOneRequest) (*pb.Product, error) {
 	return p.repository.FindOne(req)
 }
 
-func (p *ProductUsecase) Delete(req *pb.ProductDeleteRequest) (*pb.OperationResponse, error) {
-	return p.repository.Delete(req)
+func (p *ProductUsecase) FindAll(req *pb.ProductFindAllRequest) (*pb.ProductFindAllResponse, error) {
+	return p.repository.FindAll(req)
 }
 
-func (p *ProductUsecase) Post(req *pb.ProductCreateRequest) error {
-	return p.repository.Post(req)
+func (p *ProductUsecase) Update(req *pb.ProductUpdateRequest) (res *pb.OperationResponse, err error) {
+	return p.repository.Update(req)
+}
+
+func (p *ProductUsecase) Delete(req *pb.ProductDeleteRequest) (*pb.OperationResponse, error) {
+	return p.repository.Delete(req)
 }
