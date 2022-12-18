24c28ad23f8cbd4efe25a7391b920d4d6d96436d
umar <umar.dev500@gmail.com>
2022-12-18 13:50:19 +0700

    add comments

--- app/repository/product_repository.go
+++ app/repository/product_repository.go
@@ -22,6 +22,10 @@ func NewProductRepository(product pb.ProductServiceClient) domain.ProductReposit
 // defer cancel()
 // }
 
+// withTimeout is a helper function that adds a timeout to a function call.
+// It takes in a duration in seconds and a function to be called, and it creates a context
+// with a timeout based on the duration. The function is then called with the context.
+// If the duration is not specified or is 0, a default timeout of 10 seconds is used.
 func (p *ProductRepository) withTimeout(dur int, f func(context.Context)) {
 	if dur == 0 {
 		dur = 10 // set default timeout duration to 10 seconds
@@ -32,6 +36,7 @@ func (p *ProductRepository) withTimeout(dur int, f func(context.Context)) {
 	f(ctx)
 }
 
+// Update updates a product in the repository.
 func (p *ProductRepository) Update(req *pb.ProductUpdateRequest) (res *pb.OperationResponse, err error) {
 	p.withTimeout(0, func(ctx context.Context) {
 		res, err = p.product.Update(ctx, req)
@@ -39,6 +44,7 @@ func (p *ProductRepository) Update(req *pb.ProductUpdateRequest) (res *pb.Operat
 	return
 }
 
+// FindAll retrieves all products from the repository.
 func (p *ProductRepository) FindAll(req *pb.ProductFindAllRequest) (res *pb.ProductFindAllResponse, err error) {
 	p.withTimeout(15, func(ctx context.Context) {
 		res, err = p.product.FindAll(ctx, req)
@@ -46,6 +52,7 @@ func (p *ProductRepository) FindAll(req *pb.ProductFindAllRequest) (res *pb.Prod
 	return
 }
 
+// FindOne retrieves a single product from the repository.
 func (p *ProductRepository) FindOne(req *pb.ProductFindOneRequest) (res *pb.Product, err error) {
 	p.withTimeout(0, func(ctx context.Context) {
 		res, err = p.product.FindOne(ctx, req)
@@ -53,6 +60,7 @@ func (p *ProductRepository) FindOne(req *pb.ProductFindOneRequest) (res *pb.Prod
 	return
 }
 
+// Delete removes a product from the repository.
 func (p *ProductRepository) Delete(req *pb.ProductDeleteRequest) (res *pb.OperationResponse, err error) {
 	p.withTimeout(0, func(ctx context.Context) {
 		res, err = p.product.Delete(ctx, req)
@@ -60,7 +68,7 @@ func (p *ProductRepository) Delete(req *pb.ProductDeleteRequest) (res *pb.Operat
 	return
 }
 
-// Post product data to rpc server
+// Post creates a new product in the repository.
 func (p *ProductRepository) Post(req *pb.ProductCreateRequest) (err error) {
 	p.withTimeout(0, func(ctx context.Context) {
 		_, err = p.product.Create(ctx, req)
