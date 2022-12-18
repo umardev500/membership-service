ae2abccdde55a7d09edc2f7faa4a4d538b60a472
umar <umar.dev500@gmail.com>
2022-12-18 14:12:48 +0700

    change method sequence

--- app/delivery/product_delivery.go
+++ app/delivery/product_delivery.go
@@ -34,6 +34,36 @@ func (p *ProductDelivery) handleResponse(ctx *fiber.Ctx, err error, status int, 
 	return helper.ApiResponse(ctx, status, message, data)
 }
 
+func (p *ProductDelivery) Create(ctx *fiber.Ctx) error {
+	var payload = new(pb.ProductCreateRequest)
+
+	if err := ctx.BodyParser(&payload); err != nil {
+		return p.handleResponse(ctx, err, 500, "", nil)
+	}
+
+	product := &pb.ProductCreateRequest{
+		Name:        payload.Name,
+		Price:       payload.Price,
+		Duration:    payload.Duration,
+		Description: payload.Description,
+	}
+
+	err := p.usecase.Post(product)
+	return p.handleResponse(ctx, err, 200, "Create product", nil)
+}
+
+func (p *ProductDelivery) FindOne(ctx *fiber.Ctx) error {
+	var id = ctx.Params("id")
+
+	res, err := p.usecase.FindOne(&pb.ProductFindOneRequest{ProductId: id})
+	return p.handleResponse(ctx, err, 200, "Find one product", res)
+}
+
+func (p *ProductDelivery) FindAll(ctx *fiber.Ctx) error {
+	res, err := p.usecase.FindAll(&pb.ProductFindAllRequest{})
+	return p.handleResponse(ctx, err, 200, "Find all products", res)
+}
+
 func (p *ProductDelivery) Update(ctx *fiber.Ctx) error {
 	var id = ctx.Params("id")
 	var payload = new(pb.ProductUpdateRequest)
@@ -60,18 +90,6 @@ func (p *ProductDelivery) Update(ctx *fiber.Ctx) error {
 	return p.handleResponse(ctx, err, 200, "Update product", nil)
 }
 
-func (p *ProductDelivery) FindAll(ctx *fiber.Ctx) error {
-	res, err := p.usecase.FindAll(&pb.ProductFindAllRequest{})
-	return p.handleResponse(ctx, err, 200, "Find all products", res)
-}
-
-func (p *ProductDelivery) FindOne(ctx *fiber.Ctx) error {
-	var id = ctx.Params("id")
-
-	res, err := p.usecase.FindOne(&pb.ProductFindOneRequest{ProductId: id})
-	return p.handleResponse(ctx, err, 200, "Find one product", res)
-}
-
 func (p *ProductDelivery) Delete(ctx *fiber.Ctx) error {
 	var id = ctx.Params("id")
 	res, err := p.usecase.Delete(&pb.ProductDeleteRequest{ProductId: id})
@@ -84,22 +102,4 @@ func (p *ProductDelivery) Delete(ctx *fiber.Ctx) error {
 	}
 
 	return p.handleResponse(ctx, err, 200, "Delete product", nil)
-}
-
-func (p *ProductDelivery) Create(ctx *fiber.Ctx) error {
-	var payload = new(pb.ProductCreateRequest)
-
-	if err := ctx.BodyParser(&payload); err != nil {
-		return p.handleResponse(ctx, err, 500, "", nil)
-	}
-
-	product := &pb.ProductCreateRequest{
-		Name:        payload.Name,
-		Price:       payload.Price,
-		Duration:    payload.Duration,
-		Description: payload.Description,
-	}
-
-	err := p.usecase.Post(product)
-	return p.handleResponse(ctx, err, 200, "Create product", nil)
 }
