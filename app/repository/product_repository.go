ee3e5b7b47aa3d73e813e4e9a3b942ac6a6a80f4
umar <umar.dev500@gmail.com>
2022-12-18 12:46:23 +0700

    use dynamic timeout

--- app/repository/product_repository.go
+++ app/repository/product_repository.go
@@ -22,36 +22,36 @@ func NewProductRepository(product pb.ProductServiceClient) domain.ProductReposit
 // defer cancel()
 // }
 
-func (p *ProductRepository) withTimeout(f func(context.Context)) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+func (p *ProductRepository) withTimeout(dur int, f func(context.Context)) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(dur)*time.Second)
 	defer cancel()
 
 	f(ctx)
 }
 
 func (p *ProductRepository) Update(req *pb.ProductUpdateRequest) (res *pb.OperationResponse, err error) {
-	p.withTimeout(func(ctx context.Context) {
+	p.withTimeout(10, func(ctx context.Context) {
 		res, err = p.product.Update(ctx, req)
 	})
 	return
 }
 
 func (p *ProductRepository) FindAll(req *pb.ProductFindAllRequest) (res *pb.ProductFindAllResponse, err error) {
-	p.withTimeout(func(ctx context.Context) {
+	p.withTimeout(15, func(ctx context.Context) {
 		res, err = p.product.FindAll(ctx, req)
 	})
 	return
 }
 
 func (p *ProductRepository) FindOne(req *pb.ProductFindOneRequest) (res *pb.Product, err error) {
-	p.withTimeout(func(ctx context.Context) {
+	p.withTimeout(10, func(ctx context.Context) {
 		res, err = p.product.FindOne(ctx, req)
 	})
 	return
 }
 
 func (p *ProductRepository) Delete(req *pb.ProductDeleteRequest) (res *pb.OperationResponse, err error) {
-	p.withTimeout(func(ctx context.Context) {
+	p.withTimeout(10, func(ctx context.Context) {
 		res, err = p.product.Delete(ctx, req)
 	})
 	return
@@ -59,7 +59,7 @@ func (p *ProductRepository) Delete(req *pb.ProductDeleteRequest) (res *pb.Operat
 
 // Post product data to rpc server
 func (p *ProductRepository) Post(req *pb.ProductCreateRequest) (err error) {
-	p.withTimeout(func(ctx context.Context) {
+	p.withTimeout(10, func(ctx context.Context) {
 		_, err = p.product.Create(ctx, req)
 	})
 	return
