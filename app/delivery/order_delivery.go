7e17dbf67162dfd305b3491d00fdf0034306e747
umar <umar.dev500@gmail.com>
2022-12-18 19:30:20 +0700

    add filter

--- app/delivery/order_delivery.go
+++ app/delivery/order_delivery.go
@@ -22,7 +22,7 @@ func NewOrderDelivery(usecase domain.OrderUsecase, router fiber.Router) {
 
 func (o *OrderDelivery) handleResponse(ctx *fiber.Ctx, err error, status int, message string, data interface{}) error {
 	if err != nil {
-		return helper.ApiResponse(ctx, 500, err.Error(), nil)
+		return helper.ApiResponse(ctx, status, err.Error(), nil)
 	}
 	return helper.ApiResponse(ctx, status, message, data)
 }
@@ -30,8 +30,21 @@ func (o *OrderDelivery) handleResponse(ctx *fiber.Ctx, err error, status int, me
 // func (o *OrderDelivery) FindAll(ctx *fiber.Ctx) error {}
 
 func (o *OrderDelivery) FindAll(ctx *fiber.Ctx) error {
+	sort := ctx.Query("sort", "asc")
+	page := helper.ToInt(ctx.Query("page", "0"))
+	perPage := helper.ToInt(ctx.Query("per_page"))
+	search := ctx.Query("search")
+	status := ctx.Query("status")
+
 	reqContext := ctx.Context()
-	res, err := o.usecase.FindAll(reqContext, &pb.OrderFindAllRequest{})
+	filter := &pb.OrderFindAllRequest{
+		Sort:    sort,
+		Page:    page,
+		PerPage: perPage,
+		Search:  search,
+		Status:  status,
+	}
+	res, err := o.usecase.FindAll(reqContext, filter)
 
 	return o.handleResponse(ctx, err, 200, "Find all orders", res)
 }
