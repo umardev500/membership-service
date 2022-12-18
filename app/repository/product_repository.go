3f8da99ec4919c6b3417ef4fe103dcd25c0efc9d
umar <umar.dev500@gmail.com>
2022-12-18 12:42:46 +0700

    remove error return

--- app/repository/product_repository.go
+++ app/repository/product_repository.go
@@ -22,50 +22,45 @@ func NewProductRepository(product pb.ProductServiceClient) domain.ProductReposit
 // defer cancel()
 // }
 
-func (p *ProductRepository) withTimeout(f func(context.Context) error) error {
+func (p *ProductRepository) withTimeout(f func(context.Context)) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	return f(ctx)
+	f(ctx)
 }
 
 func (p *ProductRepository) Update(req *pb.ProductUpdateRequest) (res *pb.OperationResponse, err error) {
-	err = p.withTimeout(func(ctx context.Context) error {
+	p.withTimeout(func(ctx context.Context) {
 		res, err = p.product.Update(ctx, req)
-		return err
 	})
 	return
 }
 
 func (p *ProductRepository) FindAll(req *pb.ProductFindAllRequest) (res *pb.ProductFindAllResponse, err error) {
-	err = p.withTimeout(func(ctx context.Context) error {
+	p.withTimeout(func(ctx context.Context) {
 		res, err = p.product.FindAll(ctx, req)
-		return err
 	})
 	return
 }
 
 func (p *ProductRepository) FindOne(req *pb.ProductFindOneRequest) (res *pb.Product, err error) {
-	err = p.withTimeout(func(ctx context.Context) error {
+	p.withTimeout(func(ctx context.Context) {
 		res, err = p.product.FindOne(ctx, req)
-		return err
 	})
 	return
 }
 
 func (p *ProductRepository) Delete(req *pb.ProductDeleteRequest) (res *pb.OperationResponse, err error) {
-	err = p.withTimeout(func(ctx context.Context) error {
+	p.withTimeout(func(ctx context.Context) {
 		res, err = p.product.Delete(ctx, req)
-		return err
 	})
 	return
 }
 
 // Post product data to rpc server
 func (p *ProductRepository) Post(req *pb.ProductCreateRequest) (err error) {
-	err = p.withTimeout(func(ctx context.Context) error {
+	p.withTimeout(func(ctx context.Context) {
 		_, err = p.product.Create(ctx, req)
-		return err
 	})
 	return
 }
