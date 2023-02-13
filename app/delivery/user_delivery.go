dee1390a14b7d9568382c2d5896fdc9b8b64af2b
umar <umar.dev500@gmail.com>
2023-02-13 20:22:44 +0700

    initial user update detail

--- app/delivery/user_delivery.go
+++ app/delivery/user_delivery.go
@@ -1,8 +1,12 @@
 package delivery
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"membership/domain"
 	"membership/helper"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,6 +21,7 @@ func NewUserDelivery(usecase domain.UserUsecase, router fiber.Router) {
 	}
 
 	router.Get("/users/:id", handler.Find)
+	router.Put("/users/:id", handler.UpdateDetail)
 }
 
 func (u *userDelivery) handleResponse(ctx *fiber.Ctx, err error, status int, message string, data interface{}) error {
@@ -24,6 +29,58 @@ func (u *userDelivery) handleResponse(ctx *fiber.Ctx, err error, status int, mes
 		return helper.ApiResponse(ctx, 500, err.Error(), nil)
 	}
 	return helper.ApiResponse(ctx, status, message, data)
+}
+
+func (u *userDelivery) UpdateDetail(ctx *fiber.Ctx) error {
+	// Read payload
+	var userDetail domain.UserDetail
+
+	req, _ := ctx.MultipartForm()
+	payload := req.Value["payload"][0]
+	json.Unmarshal([]byte(payload), &userDetail)
+
+	// Upload file
+	file, err := ctx.FormFile("file")
+	if err != nil {
+		return u.handleResponse(ctx, err, 200, "Upload file", nil)
+	}
+
+	filePath := fmt.Sprintf("./public/uploads/images/avatars/%s", file.Filename)
+	out, err := os.Create(filePath)
+	if err != nil {
+		return u.handleResponse(ctx, err, 200, "Upload file", nil)
+	}
+
+	defer out.Close()
+
+	fileReader, err := file.Open()
+	if err != nil {
+		return u.handleResponse(ctx, err, 200, "Upload file", nil)
+	}
+
+	defer fileReader.Close()
+
+	var contents []byte
+	var buf = make([]byte, 1024)
+	for {
+		n, err := fileReader.Read(buf)
+		if err != nil && err != io.EOF {
+			return u.handleResponse(ctx, err, 200, "Upload file", nil)
+		}
+
+		if n == 0 {
+			break
+		}
+
+		contents = append(contents, buf[:n]...)
+
+	}
+
+	if _, err := out.Write(contents); err != nil {
+		return u.handleResponse(ctx, err, 200, "Upload file", nil)
+	}
+
+	return ctx.JSON("OK")
 }
 
 func (u *userDelivery) Find(ctx *fiber.Ctx) error {
--- domain/user_domain.go
+++ domain/user_domain.go
@@ -5,6 +5,22 @@ import (
 	"membership/pb"
 )
 
+type UserLocation struct {
+	Address    string `bson:"address,omitempty"`
+	Village    string `bson:"village,omitempty"`
+	District   string `bon:"district,omitempty"`
+	City       string `bson:"city,omitempty"`
+	Province   string `bson:"province,omitempty"`
+	PostalCode string `bson:"postal_code,omitempty"`
+}
+
+type UserDetail struct {
+	Name     string        `bson:"name,omitempty"`
+	Email    string        `bson:"email,omitempty"`
+	Phone    string        `bson:"phone,omitempty"`
+	Location *UserLocation `bson:"location,omitempty"`
+}
+
 type UserUsecase interface {
 	Find(ctx context.Context, userId string) (res *pb.UserFindResponse, err error)
 }
